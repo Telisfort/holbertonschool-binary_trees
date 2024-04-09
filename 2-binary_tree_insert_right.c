1ef25bbc04bac2246faaa2e18aaa8ae4608f3a9a
diarra192 <dboureima192@gmail.com>
2024-04-09 16:29:08 +0200

    dossier binary

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,37 +1,34 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+
 /**
- * binary_tree_insert_right - inserts a node as the right child
- * @parent: the parent node
- * @value: data stored in the node
- * Return: NULL if fails, the new node if success
+ * binary_tree_insert_right - Insère un nœud en tant que fils droit d'un nœud parent.
+ * @parent: Pointeur vers le nœud parent.
+ * @value: Valeur stockée dans le nouveau nœud.
+ * Return: Pointeur vers le nouveau nœud, ou NULL en cas d'échec.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+    binary_tree_t *new;
 
-	if (parent == NULL)
-		return (NULL);
+    if (!parent)
+        return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-		return (NULL);
+    new = malloc(sizeof(binary_tree_t));
+    if (!new)
+        return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
+    new->n = value;
+    new->parent = parent;
+    new->left = NULL;
+    new->right = parent->right;
 
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
-	}
+    if (parent->right)
+        parent->right->parent = new;
 
-	else
-	{
-		new_node->right = NULL;
-	}
+    parent->right = new;
 
-	parent->right = new_node;
-
-	return (new_node);
+    return (new);
 }
+
+
